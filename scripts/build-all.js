f8cebe99a55819dc4c5971749df29fc6613eb98d
Pete Patterson <peter.patterson@fsstudio.com>
2022-09-08 01:16:07 +0000

    moved files

--- scripts/build-all.js
+++
@@ -1,8 +0,0 @@
-var shell = require('shelljs');
-var forEachSample = require('./foreach-sample');
-
-forEachSample((sampleDir) => {
-	var cmd = `pushd ${sampleDir} && npm run build && popd`;
-	console.log(cmd);
-	shell.exec(cmd);
-});
